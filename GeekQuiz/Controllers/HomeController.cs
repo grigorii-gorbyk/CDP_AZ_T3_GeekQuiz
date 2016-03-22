9cf34938aabbf6d6731b7c79657222a203624d41
Grygorii Gorbyk <Grygorii.Gorbyk@wolterskluwer.com>
2016-03-22 20:18:40 +0200

    M2

--- GeekQuiz/Controllers/HomeController.cs
+++ GeekQuiz/Controllers/HomeController.cs
@@ -31,6 +31,16 @@ namespace GeekQuiz.Controllers
         //m1 m1 m1 m1 m1 m1 m1 m1 m1 m1
 
 
+        //m2 m2 m2 m2 m2 m2 m2 m2 m2 m2
+        //m2 m2 m2 m2 m2 m2 m2 m2 m2 m2
+        //m2 m2 m2 m2 m2 m2 m2 m2 m2 m2
+        //m2 m2 m2 m2 m2 m2 m2 m2 m2 m2
+        //m2 m2 m2 m2 m2 m2 m2 m2 m2 m2
+        //m2 m2 m2 m2 m2 m2 m2 m2 m2 m2
+        //m2 m2 m2 m2 m2 m2 m2 m2 m2 m2
+        //m2 m2 m2 m2 m2 m2 m2 m2 m2 m2
+
+
 
         private const string TenantIdClaimType = "http://schemas.microsoft.com/identity/claims/tenantid";
         private const string LoginUrl = "https://login.windows.net/{0}";
